40720ce2f5390579f3bdd3713830ff0bbd800675
Eray Alkış <lolpo12345@outlook.com>
2023-09-02 00:40:16 +0300

    Create base flow for lists

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use diesel::prelude::*;
+use task_rs::get_db;
+use task_rs::models::*;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -8,7 +11,14 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{}", args.todo_body);
-    println!("{}", args.list_name);
+    use task_rs::schema::lists::dsl::*;
+
+    // let args = Args::parse();
+
+    let conn = &mut get_db();
+    let res = lists.limit(5).select(List::as_select()).load(conn).unwrap();
+
+    for list in res {
+        println!("ID: {} | TITLE: {}", list.id, list.title);
+    }
 }
