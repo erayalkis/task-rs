ab40e977effee8c28af8752954d27c4d2da4858c
Eray Alkış <lolpo12345@outlook.com>
2023-09-02 19:21:52 +0300

    Create a public static DB variable with mutex and Lazy

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,15 @@ pub mod models;
 pub mod schema;
 
 use diesel::{sqlite::SqliteConnection, Connection};
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+pub static DB: Mutex<Lazy<SqliteConnection>> = Mutex::new(Lazy::new(|| get_db()));
 
 pub fn get_db() -> SqliteConnection {
     // TODO: Replace this to use the `dotenv` package instead
     let db_url = "./taskrs.db";
 
+    println!("Creating DB connection!");
     SqliteConnection::establish(db_url).expect("Error while connecting to database!")
 }
