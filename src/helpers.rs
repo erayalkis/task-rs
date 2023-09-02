19706138860a880900dd836dbc6d258bfa943729
Eray Alkış <lolpo12345@outlook.com>
2023-09-02 21:41:45 +0300

    Create toggle_task_completion

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,8 +3,10 @@ use crate::schema::tasks::dsl::*;
 use crate::{get_db, models::*};
 use diesel::prelude::*;
 
-pub fn get_list_items(list_name: &String) -> Vec<Task> {
+pub fn get_list_items(list_name: &String) -> Result<Vec<Task>, diesel::result::Error> {
     let mut conn = get_db();
+
+    // TODO: Make this part use the `first` function for Diesel
     let list: Vec<List> = lists
         .filter(title.eq(list_name))
         .limit(1)
@@ -16,10 +18,11 @@ pub fn get_list_items(list_name: &String) -> Vec<Task> {
         .filter(list_id.eq(list[0].id))
         .select(Task::as_select())
         .load(&mut conn)
-        .expect("Error while loading tasks!")
 }
 
-pub fn display_list_items(list_name: &String, content: &Vec<Task>) {
+pub fn display_list_items(list_name: &String) {
+    let content = get_list_items(list_name).unwrap();
+
     if content.len() == 0 {
         println!("List {} has no tasks assigned to it!", list_name);
         return;
@@ -29,4 +32,13 @@ pub fn display_list_items(list_name: &String, content: &Vec<Task>) {
     for task in content {
         println!("{}: {}", task.id, task.body);
     }
+}
+
+pub fn toggle_task_completion(task_id: &Option<i32>) -> Result<usize, diesel::result::Error> {
+    let mut conn = get_db();
+    let id_unwrapped: i32 = task_id.unwrap();
+
+    diesel::update(tasks.find(id_unwrapped))
+        .set(completed.eq(true))
+        .execute(&mut conn)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::Parser;
-use task_rs::helpers::{display_list_items, get_list_items};
+use task_rs::helpers::{display_list_items, toggle_task_completion};
 
 #[derive(Parser, Debug)]
 struct Args {
     command: String,
     #[arg(short, long)]
     list_name: String,
+    task_id: Option<i32>,
 }
 
 fn main() {
@@ -13,11 +14,16 @@ fn main() {
 
     match args.command.as_str() {
         "ls" => {
-            let content = get_list_items(&args.list_name);
-            display_list_items(&args.list_name, &content);
+            display_list_items(&args.list_name);
         }
         "edit" => {}
-        "toggle" => {}
-        _ => {}
+        "toggle" => {
+            toggle_task_completion(&args.task_id).unwrap();
+        }
+        _ => {
+            if args.list_name.len() == 0 {
+                panic!("Cannot create a task without the <LIST_NAME> parameter!");
+            }
+        }
     }
 }
