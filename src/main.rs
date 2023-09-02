b84769c212dcd73475433e6a22243b4dbe645d55
Eray Alkış <lolpo12345@outlook.com>
2023-09-02 18:54:14 +0300

    Create a basic pattern match for args

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 use clap::Parser;
-use diesel::prelude::*;
-use task_rs::get_db;
-use task_rs::models::*;
 
 #[derive(Parser, Debug)]
 struct Args {
-    todo_body: String,
+    command: String,
     #[arg(short, long)]
     list_name: String,
 }
 
 fn main() {
-    use task_rs::schema::lists::dsl::*;
-
     let args = Args::parse();
 
-    let conn = &mut get_db();
-    let res = lists.limit(5).select(List::as_select()).load(conn).unwrap();
-
-    for list in res {
-        println!("ID: {} | TITLE: {}", list.id, list.title);
+    match args.command.as_str() {
+        "ls" => {}
+        "edit" => {}
+        "toggle" => {}
+        _ => {}
     }
 }
